5d9fab8722d8894a7937fddb5d3976d5308a3762
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-22 21:25:37 +0900

    <h1>タグを表示する

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDom from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 const App = () => {
-  return null;
+  return <h1>こんにちは！</h1>;
 };
 
-ReactDom.createRoot(document.getElementById('root')).render(
-  <App />,
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
