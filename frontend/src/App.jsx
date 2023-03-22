ddeb906e0cfce55794165b3c26290921689a74e9
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-22 21:33:04 +0900

    Appコンポーネントを作成

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <p>aaa</p>
+    </>
+  )
+}
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは！</h1>
-      <p>aaa</p>
-    </>
-  )
-};
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
