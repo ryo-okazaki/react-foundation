e391307b7abe7540aec586814f4ecf5703223698
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-22 21:36:20 +0900

    ボタン操作実現

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+
   return (
     <>
       <h1>こんにちは！</h1>
       <p>aaa</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   )
 }
