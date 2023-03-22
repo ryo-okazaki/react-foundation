902993901d5f5e429d5ef0745f8f6aedc1ef0b8d
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-22 21:39:42 +0900

    スタイルを直接当てる

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const App = () => {
   const onClickButton = () => alert();
+  const contentType = {
+    color: 'blue',
+    fontSize: '18px',
+  }
 
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>aaa</p>
+      <h1 style={{ color: 'red' }}>こんにちは！</h1>
+      <p style={contentType}>aaa</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   )
