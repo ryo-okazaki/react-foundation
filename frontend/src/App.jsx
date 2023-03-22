53b9a53a8d2ebe2620b294c72eb18f5143c015aa
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-23 00:40:53 +0900

    軽微なリファクタリング

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React, {useState} from "react";
 import ColorfulMessage from "./components/ColorfulMessage.jsx";
 
 const App = () => {
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
   const [num, setNum] = useState(0); // useStateの中は初期値
   // stateとして使用する変数名、2つ目はそのstateを変更するための関数名
 
   const [showFlag, setShowFlag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
 
   const onClickSwitchShowFlag = () => {
     setShowFlag(!showFlag);
