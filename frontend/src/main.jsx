8fa1cefacf006792296293ce9c1fb1602289d5bc
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-22 21:11:14 +0900

    react開発の基盤を構築

---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+const App = () => {
+  return null;
+};
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <App />,
+);
